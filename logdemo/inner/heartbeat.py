9eedada22a6f175c3ceec2a51b518ee996b58ce3
hzhuzheng <hzhuzheng@corp.netease.com>
2015-09-15 13:38:50 +0800

    only you

--- logdemo/inner/heartbeat.py
+++ logdemo/inner/heartbeat.py
@@ -6,4 +6,7 @@ LOG = logging.getLogger(__name__)
 def start():
     while True:
         LOG.info('hello, I love you.')
+        LOG.debug('debug , I hate you.')
+        LOG.warn('warn, I do not like you')
+        LOG.error('error, I fuck you')
         time.sleep(1)
